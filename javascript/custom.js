0be0e27022f3c7df922c5f206bffca5419864520
unlimiteddigits <one.poorman@protonmail.com>
2024-04-28 22:28:01 -0400

    Var

--- javascript/custom.js
+++ javascript/custom.js
@@ -1,2 +1,7 @@
+const { popupWindow } = require("dcent-web-connector");
+
 console.log("Hi World");
 console.log("Hi Again");
+var x = 10;
+popupWindow(x.toString);
+document.getElementById("myText").innerHTML = x;
