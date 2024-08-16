1063770807ab2288e8c5622c93610cb9b027fa01
unlimiteddigits <one.poorman@protonmail.com>
2024-08-16 09:43:20 -0400

    added baseline code

---
+++ javascript/saveme.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo $@
+
+if [[ $# == 1 ]]; then
+	git add *;
+	git commit -m "$@";
+	git push;
+else
+	echo "failed to add a comment so we are running 'chmod a+x *'"
+	chmod "a+x *"
+fi
