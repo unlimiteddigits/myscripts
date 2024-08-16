e86d1f32543891e1542fc5653c2be8def891d13f
unlimiteddigits <one.poorman@protonmail.com>
2024-08-16 08:44:22 -0400

    javascript added

--- javascript/custom.js
+++ javascript/custom.js
@@ -1,7 +1,21 @@
-const { popupWindow } = require("dcent-web-connector");
 
 console.log("Hi World");
 console.log("Hi Again");
-var x = 10;
-popupWindow(x.toString);
-document.getElementById("myText").innerHTML = x;
+var x = 90;
+
+function myOnloadFunction(){
+    document.getElementById('myText').innerHTML = 'getElementByID from inside custom.js';
+  }
+
+function getDateAndTime(){
+    document.getElementById('myText').innerHTML = Date();
+}
+
+function listPrimeNumbers(numberRequsted){
+    document.getElementById('myText').innerHTML = "";
+    for (i=1; i<=numberRequsted; i++){ 
+    document.getElementById('myText').innerHTML += i + '<br/>';
+    //alert(`Looping ${i}`);
+    }
+    x = 5;
+}
