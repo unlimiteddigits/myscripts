75f5cf0581623d0d03672870c9992072d438dd17
unlimiteddigits <one.poorman@protonmail.com>
2024-09-22 22:23:39 -0400

    Closer

--- javascript/REST/Learn.js
+++ javascript/REST/Learn.js
@@ -1,5 +1,20 @@
+const { restClient } = require('@polygon.io/client-js');
+const rest = restClient("Lv_E905oWriVZpHHAOXkkq3FQfZ28yKd");
+
 console.log("Hello")
 
 function myOnloadFunction(){
-    document.getElementById('myText').innerHTML = 'getElementByID from inside Learn.js';
-  }
+  document.getElementById('myText').innerHTML = "Press the button to check XRPs price yesterday.";
+}
+
+function CheckXRP(){
+  document.getElementById('myText').innerHTML = 'Checking';
+
+  rest.stocks.aggregates("AAPL", 1, "day", "2023-01-01", "2019-04-14").then((data) => {
+    console.log(data);
+    document.getElementById('myText').innerHTML=data;
+  }).catch(e => {
+    console.error('An error happened:', e);
+    document.getElementById('myText').innerHTML="An error happened:";
+  });
+}
